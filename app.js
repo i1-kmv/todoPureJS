17ec2932ee90bee4c9706b4258462b97a0078aaf
i1-kmv <i1-kmv@mail.ru>
2020-11-30 20:55:51 +0300

    step1 ready

--- app.js
+++ app.js
@@ -31,4 +31,88 @@ const tasks = [
   },
 ];
 
-(function(arrOfTasks) {})(tasks);
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task
+    return acc
+  }, {})
+
+// Elements UI
+
+  const listContainer = document.querySelector('.tasks-list-section .list-group')
+  const form = document.forms['addTask']
+  const inputTitle = form.elements['title']
+  const inputBody = form.elements['body']
+
+
+// Events
+  renderAllTasks(objOfTasks)
+  form.addEventListener('submit', onFormSubmitHandler)
+
+  function renderAllTasks(tasksList) {
+    if ( !tasksList) {
+      console.error('Передайте список задач')
+      return
+    }
+
+    const fragment = document.createDocumentFragment()
+    Object.values(tasksList).forEach(task => {
+      const li = listItemTemplate(task)
+      fragment.appendChild(li)
+    })
+    listContainer.appendChild(fragment)
+  }
+
+  function listItemTemplate({_id, title, body} = {}) {
+    const li = document.createElement('li')
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2')
+
+    const span = document.createElement('span')
+    span.textContent = title
+    span.style.fontWeight = 'bold'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'Delete task'
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn')
+
+    const article = document.createElement('p')
+    article.textContent = body
+    article.classList.add('mt-2', 'w-100') 
+
+    li.appendChild(span)
+    li.appendChild(deleteBtn)
+    li.appendChild(article)
+
+    return li
+
+  }
+
+  function onFormSubmitHandler(e){
+    e.preventDefault()
+    const titleValue = inputTitle.value
+    const bodyValue = inputBody.value
+
+    if(!titleValue || !bodyValue) {
+      alert('Введите title и body')
+      return
+    }
+
+    const task = createNewTask(titleValue, bodyValue)
+    const listItem = listItemTemplate(task)
+    listContainer.insertAdjacentElement('afterbegin', listItem)
+    form.reset()
+  }
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`
+    }
+
+    objOfTasks[newTask._id] = newTask
+
+    return { ...newTask }
+  }
+} )(tasks);
